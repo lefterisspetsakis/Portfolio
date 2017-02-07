a4b4569506e278de52a7018fb39bcc23da24bd26
Lefteris Spetsakis <Lefteris Spetsakis>
2017-02-07 00:30:49 -0500

    Added better js event handling

--- src/scripts/core.js
+++ src/scripts/core.js
@@ -14,31 +14,30 @@ function respTopNav()
 
 function changeToPage()
 {
-//  // Wait for the page to load first
-//  window.onload = function() {
-//
-//    //Get a reference to the link on the page
-//    // with an id of "mylink"
-//    var a = document.getElementById("mylink");
-//
-//    //Set code to run when the link is clicked
-//    // by assigning a function to "onclick"
-//    a.onclick = function() {
-//
-//      // Your code here...
-//
-//      //If you don't want the link to actually 
-//      // redirect the browser to another page,
-//      // "google.com" in our example here, then
-//      // return false at the end of this block.
-//      // Note that this also prevents event bubbling,
-//      // which is probably what we want here, but won't 
-//      // always be the case.
-//      return false;
-//    }
-//  }
-  document.getElementById("documentArea").setAttribute('data', 'projects.html');
-//  var obj = document.getElementById("documentArea");
-//  var content = obj.innerHTML;
-//  obj.innerHTML= content;
+  window.location.replace("projects.html");
+  window.onload = function()
+  {
+
+    //Get a reference to the link on the page
+    // with an id of "mylink"
+    var a = document.getElementById("documentArea");
+
+    //Set code to run when the link is clicked
+    // by assigning a function to "onclick"
+    a.onclick = function()
+    {
+
+      // Your code here...
+
+      //If you don't want the link to actually 
+      // redirect the browser to another page,
+      // "google.com" in our example here, then
+      // return false at the end of this block.
+      // Note that this also prevents event bubbling,
+      // which is probably what we want here, but won't 
+      // always be the case.
+      return false;
+    }
+  }
+  window.alert(document.getElementById("documentArea"));
 }
