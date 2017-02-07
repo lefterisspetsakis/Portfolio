1211b31d13596c48725555b7d3e82299dbebfed5
Lefteris Spetsakis <LefterisSpetsakis@saintelizabeth.com>
2017-02-07 11:12:13 -0500

    Navbar moved to index.html. Navbar links work

--- src/scripts/core.js
+++ src/scripts/core.js
@@ -12,32 +12,7 @@ function respTopNav()
   }
 }
 
-function changeToPage()
+function changeToPage(new_url)
 {
-  window.location.replace("projects.html");
-  window.onload = function()
-  {
-
-    //Get a reference to the link on the page
-    // with an id of "mylink"
-    var a = document.getElementById("documentArea");
-
-    //Set code to run when the link is clicked
-    // by assigning a function to "onclick"
-    a.onclick = function()
-    {
-
-      // Your code here...
-
-      //If you don't want the link to actually 
-      // redirect the browser to another page,
-      // "google.com" in our example here, then
-      // return false at the end of this block.
-      // Note that this also prevents event bubbling,
-      // which is probably what we want here, but won't 
-      // always be the case.
-      return false;
-    }
-  }
-  window.alert(document.getElementById("documentArea"));
+  document.getElementById("documentArea").setAttribute('data', new_url);
 }
